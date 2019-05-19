db073aa22af6cb1998ffddb3195b7c496021894f
Andrea Flores <andrea94flores@gmail.com>
2019-05-19 08:56:32 -0500

    adds bmi calculator

--- calculator.js
+++ calculator.js
@@ -11,14 +11,23 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    var num1 = parseInt(req.body.num1);
-    var num2 = parseInt(req.body.num2);
-    var result = num1 + num2;
+    let num1 = parseInt(req.body.num1);
+    let num2 = parseInt(req.body.num2);
+    let result = num1 + num2;
 
     res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+});
 
+app.post("/bmicalculator", function(req, res){
+    let weight = (req.body.weight);
+    let height = (req.body.height);
+    let result = (weight/(Math.pow(height, 2))) * 703;
+    res.send("Your BMI is " + Math.round(result));
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
