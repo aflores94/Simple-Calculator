c665a464618693eed94061445507080a01dc5e5a
Andrea Flores <andrea94flores@gmail.com>
2019-05-20 09:10:09 -0500

    adds dog year calculator

--- calculator.js
+++ calculator.js
@@ -11,8 +11,8 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    let num1 = parseInt(req.body.num1);
-    let num2 = parseInt(req.body.num2);
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
     let result = num1 + num2;
 
     res.send("The result of the calculation is " + result);
@@ -22,11 +22,26 @@ app.get("/bmicalculator", function(req, res){
     res.sendFile(__dirname + "/bmicalculator.html");
 });
 
-app.post("/bmicalculator", function(req, res){
+app.post("/bmicalculator", function (req, res) {
     let weight = (req.body.weight);
     let height = (req.body.height);
-    let result = (weight/(Math.pow(height, 2))) * 703;
+    let result = (weight / (Math.pow(height, 2))) * 703;
     res.send("Your BMI is " + Math.round(result));
+});
+
+app.get("/dogyears", function(req, res) {
+    res.sendFile(__dirname + "/dogyears.html");
+});
+
+app.post("/dogyears", function (req, res) {
+    let dogAge = (req.body.dogAge);
+    if (dogAge === 1) {
+        res.send("Your dog is 15 in human years.");
+    } else if (dogAge === 2) {
+        res.send("Your dog is 24 in human years.");
+    } else {
+        res.send("Your dog is " + (24 + ((dogAge - 2) * 5)) + " in human years.");
+    }
 });
 
 app.listen(3000, function() {
